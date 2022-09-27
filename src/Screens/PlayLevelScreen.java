99eb77e08da454dd51df6a4a32331000c25b4617
Ajifolahan <Ajifolahan>
2022-09-27 17:38:54 -0400

    Players health display

--- src/Screens/PlayLevelScreen.java
+++ src/Screens/PlayLevelScreen.java
@@ -31,6 +31,7 @@ public class PlayLevelScreen extends Screen implements PlayerListener {
 
     public PlayLevelScreen(ScreenCoordinator screenCoordinator) {
         this.screenCoordinator = screenCoordinator;
+       
     }
 
     public void initialize() {
@@ -48,7 +49,8 @@ public class PlayLevelScreen extends Screen implements PlayerListener {
 
         levelClearedScreen = new LevelClearedScreen();
         levelLoseScreen = new LevelLoseScreen(this);
-        healthdisplay = new HealthDisplay("LIVES:", 650, 50, "Comic Sans", 20, Color.red);
+        this.player.getPlayerhealth();//......................//
+        healthdisplay = new HealthDisplay("LIVES: " + player.getPlayerhealth(), 650, 50, "Comic Sans", 20, Color.red);
         timedisplay= new TimeDisplay("TIME TAKEN:", 450, 50, "Comic Sans", 20, Color.red);
     }
 
@@ -58,6 +60,7 @@ public class PlayLevelScreen extends Screen implements PlayerListener {
             // if level is "running" update player and map to keep game logic for the platformer level going
             case RUNNING:
                 player.update();
+//                player.getPlayerhealth();
                 map.update(player);
                 break;
             // if level has been completed, bring up level cleared screen
