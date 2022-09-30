a9580dbd87c94189d07312feb4ac3d0e33df9594
lucasmore244 <lucas.morello@quinnipiac.edu>
2022-09-30 16:37:20 -0400

    minor changes

--- src/Maps/TestMap.java
+++ src/Maps/TestMap.java
@@ -4,9 +4,11 @@ import Enemies.RatEnemy;
 import Enemies.UFO;
 import Enemies.DinosaurEnemy;
 import Engine.ImageLoader;
+import EnhancedMapTiles.Coin;
 import EnhancedMapTiles.Checkpoint;
 import EnhancedMapTiles.EndLevelBox;
 import EnhancedMapTiles.HorizontalMovingPlatform;
+import EnhancedMapTiles.Mushrooms;
 import GameObject.Rectangle;
 import Level.*;
 import NPCs.Walrus;
@@ -34,7 +36,16 @@ public class TestMap extends Map {
         enemies.add(new RatEnemy(getMapTile(15, 8).getLocation().addY(20), Direction.LEFT));
         enemies.add(new DinosaurEnemy(getMapTile(19, 1).getLocation().addY(2), getMapTile(22, 1).getLocation().addY(2), Direction.RIGHT));
         enemies.add(new DinosaurEnemy(getMapTile(58, 4).getLocation().addY(2), getMapTile(60, 4).getLocation().addY(2), Direction.RIGHT));
-     
+        enemies.add(new Mushrooms(getMapTile(16, 8).getLocation()));
+        
+        enemies.add(new Mushrooms(getMapTile(24, 10).getLocation()));
+        
+        enemies.add(new Mushrooms(getMapTile(34, 10).getLocation()));
+        
+        enemies.add(new Mushrooms(getMapTile(54, 10).getLocation()));
+      
+        enemies.add(new Mushrooms(getMapTile(63, 11).getLocation()));
+        
         return enemies;
     }
 
@@ -55,7 +66,17 @@ public class TestMap extends Map {
         enhancedMapTiles.add(new HorizontalMovingPlatform(
                 ImageLoader.load("GreenPlatform.png"),
                 getMapTile(36, 8).getLocation(),
-                getMapTile(42, 8).getLocation(),
+                getMapTile(43, 8).getLocation(),
+                TileType.JUMP_THROUGH_PLATFORM,
+                3,
+                new Rectangle(0, 6,16,4),
+                Direction.LEFT
+        ));
+        
+        enhancedMapTiles.add(new HorizontalMovingPlatform(
+                ImageLoader.load("GreenPlatform.png"),
+                getMapTile(41, 8).getLocation(),
+                getMapTile(51, 8).getLocation(),
                 TileType.JUMP_THROUGH_PLATFORM,
                 3,
                 new Rectangle(0, 6,16,4),
@@ -64,27 +85,33 @@ public class TestMap extends Map {
         
         enhancedMapTiles.add(new HorizontalMovingPlatform(
                 ImageLoader.load("GreenPlatform.png"),
-                getMapTile(42, 8).getLocation(),
-                getMapTile(49, 8).getLocation(),
-                TileType.JUMP_THROUGH_PLATFORM,
-                3,
-                new Rectangle(0, 6,16,4),
-                Direction.RIGHT
-        ));
-        
-        enhancedMapTiles.add(new HorizontalMovingPlatform(
-                ImageLoader.load("GreenPlatform.png"),
-                getMapTile(49, 8).getLocation(),
+                getMapTile(48, 8).getLocation(),
                 getMapTile(54, 8).getLocation(),
                 TileType.JUMP_THROUGH_PLATFORM,
                 3,
                 new Rectangle(0, 6,16,4),
-                Direction.RIGHT
+                Direction.LEFT
         ));
 
         enhancedMapTiles.add(new EndLevelBox(
                 getMapTile(64, 10).getLocation()
         ));
+        
+        enhancedMapTiles.add(new Coin(getMapTile(7, 11).getLocation(), this));
+      
+        enhancedMapTiles.add(new Coin(
+        		getMapTile(20, 0).getLocation(), this
+        		
+        )); 
+        enhancedMapTiles.add(new Coin(
+        		getMapTile(44, 5).getLocation(), this
+        		
+        )); 
+        enhancedMapTiles.add(new Coin(
+        		getMapTile(58, 11).getLocation(), this
+        ));
+        
+        enhancedMapTiles.add(new Checkpoint(getMapTile(32, 10).getLocation(), this)); 
 
         return enhancedMapTiles; 
     }
@@ -106,6 +133,7 @@ public class TestMap extends Map {
 
         return npcs;
     }
+    
     
     public void setPlayerStartPosition(Point x) {
     	playerStartPosition = x;
