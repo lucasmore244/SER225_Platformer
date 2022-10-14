6b7ee826a51aebce7652dccd866ceff6260dcb96
SJGriffin21 <sjgriffin@quinnipiac.edu>
2022-10-14 18:10:04 -0400

    Added Loaders

--- src/Maps/Level2.java
+++ src/Maps/Level2.java
@@ -1,7 +1,23 @@
 package Maps;
 
+import java.util.ArrayList;
+
+import Enemies.DinosaurEnemy;
+import Enemies.RatEnemy;
+import Enemies.UFO;
+import Engine.ImageLoader;
+import EnhancedMapTiles.Checkpoint;
+import EnhancedMapTiles.Coin;
+import EnhancedMapTiles.HorizontalMovingPlatform;
+import EnhancedMapTiles.Mushrooms;
+import EnhancedMapTiles.Spaceship;
+import GameObject.Rectangle;
+import Level.Enemy;
+import Level.EnhancedMapTile;
 import Level.Map;
+import Level.TileType;
 import Tilesets.SpaceTileset;
+import Utils.Direction;
 
 public class Level2 extends Map{
 
@@ -9,7 +25,22 @@ public class Level2 extends Map{
         super("level2.txt", new SpaceTileset());
         this.playerStartPosition = getMapTile(1, 2).getLocation(); 
     }
+	 @Override
+	public ArrayList<Enemy> loadEnemies() {
+		 ArrayList<Enemy> enemies = new ArrayList<>();
+       
+      
+		 return enemies;
+	    }
+	 
+	 
+	 @Override
 
+	 public ArrayList<EnhancedMapTile> loadEnhancedMapTiles() {
+		 ArrayList<EnhancedMapTile> enhancedMapTiles = new ArrayList<>();
+
+		 return enhancedMapTiles; 
+	    } 
 	@Override
 	public int getCoinCount() {
 		// TODO Auto-generated method stub
