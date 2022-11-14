8464967ec5d1dcebfe0b4478678f2648a2409241
Ajifolahan <momoreajifolahan@gmail.com>
2022-11-13 19:33:12 -0500

    Created Instructions Screen

--- src/Screens/PlayLevelScreen.java
+++ src/Screens/PlayLevelScreen.java
@@ -70,7 +70,7 @@ public class PlayLevelScreen extends Screen implements PlayerListener {
 	protected SpriteFont coins, doglives;
 	protected String coincount;
 	public DisplayTime timer = new DisplayTime();
-	protected int currentMap = 1;
+	protected int currentMap = 4;
 	protected Key SHOOT_KEY = Key.Q;
 	protected Sound sound = new Sound();
 	protected MusicPanel musicPanel;
@@ -247,6 +247,7 @@ public class PlayLevelScreen extends Screen implements PlayerListener {
 			playSE(2);
 		}
 		if (playLevelScreenState == PlayLevelScreenState.LEVEL_COMPLETED && getCurrentMap() == 4) {
+			stopMusic();
 			levelClearedScreen.update();
 //			playSE(2);
 		}
